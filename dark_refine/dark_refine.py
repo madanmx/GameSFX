bbdd5a30ec8cad2e76be557e9f0d6a8eab9322cc
madanmx <madan.mx@gmail.com>
2023-06-15 19:40:35 +0200

    Update dark_refine.py

--- dark_refine/dark_refine.py
+++ dark_refine/dark_refine.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #############################################################
-#							     #	 
+#							                                #	 
 #                                                           #
 #  (c) 2023 Madan Kumar Shankar <madan.shankar@kemi.uu.se>  #
 #   Version Monday 12th June 2023 15:00 CEST                #  
@@ -15,14 +15,14 @@ import numpy as np
 from math import sqrt
 
 #USAGE:Make the directory dark_refine and copy the following files to this directory:
-# 1. FOBS_dark.mtz  (The dark reflection file)
+# 1. dark.mtz  (The dark reflection file)
 # 2. dark_start.pdb (The intial dark.pdb from the previous experiment)
 # 3. cif of the cofactor/lignad (ex: LBV.cif)
 # 4. Set the resolution RESO <low-res> <high-res> (ex:RESO 60 2.05)
 
 
 refmac = (
-    f"refmac5 HKLIN FOBS_dark.mtz HKLOUT refine.mtz XYZIN dark_start.pdb XYZOUT dark.pdb LIBIN <file.cif> << eof > refmac_log \n"
+    f"refmac5 HKLIN dark.mtz HKLOUT dark_phases.mtz XYZIN dark_start.pdb XYZOUT dark.pdb LIBIN <file.cif> << eof > refmac_log \n"
     f"REFI TYPE RESTRAINED \n" 
     f"RESO <low-res> <high-res> \n"
     f"REFI RESI MLKF \n"
@@ -34,4 +34,4 @@ refmac = (
     )
 subprocess.Popen(refmac, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).wait() 
 
-#Check the dark.pdb output from this script in coot then fit the residues/water/ligands if necessary and reun this script by changing the input
+#Check the dark.pdb output from this script in coot then fit the residues/water/ligands if necessary and rerun this script by changing the input pdb name
